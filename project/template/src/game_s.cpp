175b183cebb72cb8acd5c428176299f292efa6aa
Théo <theodau@orange.fr>
2020-12-27 18:11:29 +0100

    Tests non concluants

--- project/template/src/game_s.cpp
+++ project/template/src/game_s.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <glimac/SDLWindowManager.hpp>
 #include <glimac/glm.hpp>
@@ -7,7 +8,6 @@
 #include "game.hpp"
 
 namespace glimac {
-    /*constructeur*/
     Game::Game(char** argv) :
     // On créé une camera
     m_camera(*this),
@@ -31,7 +31,7 @@ namespace glimac {
 
     void Game::loop(){
 
-        /*BOUCLE D'AFFICHAGE*/
+        // BOUCLE D'AFFICHAGE 
 
         // Application loop:
         bool done = false;
@@ -63,14 +63,13 @@ namespace glimac {
         //Chargement des objets
 		ourModel = new Model("../project/template/models/nanosuit.obj");
         //ourModel = new Model("../project/template/models/Corridor_Left.obj");
-        m_ProjMatrix = glm::perspective(glm::radians(70.f), 800.f/600.f, 0.1f, 100.f);
 
-        /*********************************
+        /* ********************************
 		 * HERE SHOULD COME THE INITIALIZATION CODE
-		 *********************************/
+		 ********************************* /
         
 		// On charge les shaders
-        /*
+        
 		Program program = loadProgram(
 			m_applicationPath.dirPath() + "shaders/3D.vs.glsl",
 			m_applicationPath.dirPath() + "shaders/directionallights.fs.glsl"
@@ -85,10 +84,10 @@ namespace glimac {
 		m_uKs = glGetUniformLocation(program.getGLId(), "uKs");
 		m_uShininess = glGetUniformLocation(program.getGLId(), "uShininess");
 		m_uLightDir_vs = glGetUniformLocation(program.getGLId(), "uLightDir_vs");
-		m_uLightIntensity = glGetUniformLocation(program.getGLId(), "uLightIntensity");*/
+		m_uLightIntensity = glGetUniformLocation(program.getGLId(), "uLightIntensity");
         
 
-        /*
+
 		const GLuint VERTEX_ATTR_POSITION = 0;
 		glEnableVertexAttribArray(VERTEX_ATTR_POSITION); //le 0 correspond à l'attribut de position
 
@@ -109,23 +108,24 @@ namespace glimac {
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 		// On débind le VAO de la cible
-		glBindVertexArray(0);*/
+		glBindVertexArray(0);
         
     }
 
 
     //draw RENDERING
     void Game::draw(){
-        m_camera.move();
-
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        shader->Use();
+        glBindVertexArray(vao);
+        //shader->Use();
+        m_camera.move();
 
+        m_ProjMatrix = glm::perspective(glm::radians(70.f), 800.f/600.f, 0.1f, 100.f);
         m_cameraViewMatrix = m_camera.getViewMatrix();
-        std::cout << m_cameraViewMatrix << std::endl;
 
+        //Tests
         glUniformMatrix4fv(glGetUniformLocation(shader->Program, "projection"), 1, GL_FALSE, glm::value_ptr(m_ProjMatrix));
         glUniformMatrix4fv(glGetUniformLocation(shader->Program, "view"), 1, GL_FALSE, glm::value_ptr(m_cameraViewMatrix));
         
@@ -133,10 +133,32 @@ namespace glimac {
         model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f));
         model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
         glUniformMatrix4fv(glGetUniformLocation(shader->Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        ourModel->Draw(*shader);
+        //ourModel->Draw(*shader);
+
+        
+        m_earthMVMatrix = glm::rotate(m_cameraViewMatrix, m_window.getTime(), glm::vec3(0.0, 1.0, 0.0));
+        m_earthMVMatrix = glm::scale(m_cameraViewMatrix, glm::vec3(0.2, 0.2, 0.2));
+
+        
+        glUniform3f(m_uKd, 0.5, 0.5, 0.5);
+        glUniform3f(m_uKs, 0.5, 0.0, 0.0);
+        glUniform1f(m_uShininess, 32.0);
+        glm::vec4 LightDir = m_cameraViewMatrix*glm::vec4(1.0,1.0,1.0,0.);
+        glUniform3f(m_uLightDir_vs, LightDir.x, LightDir.y, LightDir.z);
+        glUniform3f(m_uLightIntensity, 3.0,3.0,3.0);
+        
+        glUniformMatrix4fv(m_uMVMatrix, 1, GL_FALSE, glm::value_ptr(m_earthMVMatrix));
+        glUniformMatrix4fv(m_uNormalMatrix, 1, GL_FALSE, glm::value_ptr(glm::transpose(glm::inverse(m_earthMVMatrix))));
+        glUniformMatrix4fv(m_uMVPMatrix, 1, GL_FALSE, glm::value_ptr(m_ProjMatrix*m_earthMVMatrix));
+
+		ourModel->Draw(*shader);
+        //ourModel2->Draw(*shader);
+
+        glBindVertexArray(0);
 
         // Update the display
         m_window.swapBuffers();
     }
 
 }
+*/
